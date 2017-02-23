c18e23588eab3f368cac316ccecc65a64c33bcf2
Zongchen <zteng@ucsd.edu>
2017-02-23 00:39:31 -0800

    lab7

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+
 var projects = require('../projects.json');
 
 /*
@@ -5,5 +6,11 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+    projects['min'] = false;
   	res.render('index', projects);
+};
+
+exports.viewMin = function (req, res) {
+    projects['min'] = true;
+    res.render('index', projects);
 };
